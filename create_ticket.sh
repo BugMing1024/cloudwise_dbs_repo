2d9cfb625af9ef71d7e58dc14167985aacab0bd2
BugMing <90375714+BugMing1024@users.noreply.github.com>
2024-09-25 00:58:48 +0800

    Update create_ticket.sh

--- create_ticket.sh
+++ create_ticket.sh
@@ -1,4 +1,5 @@
 DATE=`date`
+content="DATE: $DATE\nDRONE_BRANCH: $DRONE_BRANCH\nGIT_COMMITTER_NAME: $GIT_COMMITTER_NAME\nDRONE_GIT_SSH_URL: $DRONE_GIT_SSH_URL"
 curl --location 'https://demo.cloudwise.sg/gatewayApi/dosm/api/v2/open/api/orderCreate' \
 --header 'AppKey: CwmR8qaYCwmR' \
 --header 'AppSecret: xRFmu27Lv4t2oEeWNmQBk79eyNfltpVK' \
@@ -7,8 +8,8 @@ curl --location 'https://demo.cloudwise.sg/gatewayApi/dosm/api/v2/open/api/order
     "processDefKey": "cgtlouop",
     "formData": {
         "urgentLevel_value": "中",
-        "title": "'$CI_COMMIT_MESSAGE'",
-        "TEXTAREA_QeqUv0JtNxL": "DATE: '$DATE'\nDRONE_BRANCH: '$DRONE_BRANCH'\nGIT_COMMITTER_NAME: '$GIT_COMMITTER_NAME'\nDRONE_GIT_SSH_URL: '$DRONE_GIT_SSH_URL'",
+        "title": "$CI_COMMIT_MESSAGE",
+        "TEXTAREA_QeqUv0JtNxL": "$content",
         "stepIndex": 0,
         "urgentLevel": "1"
     },
